cc952b8f3e326dada8bfa49ba162941215e5e3e2
Yashkir Ramsamy - RMSYAS003 <rmsyas003@myuct.ac.za>
2020-06-01 16:30:49 +0200

    Added docs and fixed random seq. generation

--- paging.py
+++ paging.py
@@ -1,6 +1,9 @@
 # Yashkir Ramsamy
 # Assignment 3
 # CSC3002F
+# FIFO, LRU, OPT Page Replacement Algorithms
+# Usage: python paging.py [Frame size] [Number of Pages]
+# Usage example: python paging.py 5 64
 
 import sys
 import random
@@ -10,6 +13,9 @@ from functools import lru_cache
 
 
 def main():
+    '''
+    Runs page replacement algorithms
+    '''
     size = int(sys.argv[1])
     numPage = int(sys.argv[2])
     pages = processSequence(numPage)
@@ -23,45 +29,48 @@ def main():
 
 
 def FIFO(size, pages):
+    ''' FIFO Page Replacement algorithm'''
     frameSize = size
     pagesSeq = pages
     memoryQueue = queue.Queue(frameSize)
     fault = 0;
-    for page in pagesSeq:
-        if page not in deque(memoryQueue.queue) and not memoryQueue.full():
+    for page in pagesSeq: # Iterate through pages in sequence
+        if page not in deque(memoryQueue.queue) and not memoryQueue.full(): # memory empty or has empty slots
             memoryQueue.put(page)
             fault += 1
-        elif page in deque(memoryQueue.queue):
+        elif page in deque(memoryQueue.queue): # page exists in memory
             pass
-        elif page not in deque(memoryQueue.queue) and memoryQueue.qsize():
+        elif page not in deque(memoryQueue.queue) and memoryQueue.qsize(): # memory full and page replacement occurs
             fault += 1
-            memoryQueue.get()
-            memoryQueue.put(page)
+            memoryQueue.get() # pop page from queue
+            memoryQueue.put(page) # insert page into queue
 
     return fault
 
 
 def LRU(size, pages):
+    ''' LRU Page Replacement algorithm'''
     pageSeq = pages
 
-    @lru_cache(size)
-    def lruOperations(page):
+    @lru_cache(size) # Set the size of the LRU cache structure
+    def lruOperations(page): # Insert page into LRU Cache
         return page
 
-    for page in pageSeq:
+    for page in pageSeq: # For every page in sequence, insert into cache structure
         lruOperations(page)
-        cacheTable = lruOperations.cache_info()
-    return cacheTable[1]
+        cacheTable = lruOperations.cache_info() # Calculate and display information about cache structure
+    return cacheTable[1] # Select faults from cache structure
 
 
 
 def OPT(size, pages):
+    '''OPT Page Replacement algorithm'''
     frameSize = size
     pagesSeq = pages
     memoryQueue = deque([])
     fault = 0
     i=0
-    for page in pagesSeq:
+    for page in pagesSeq: # Iterate through pages in sequence
         if page not in memoryQueue and len(memoryQueue)<frameSize: # memory empty or has empty slots
             memoryQueue.append(page)
             fault += 1
@@ -69,9 +78,9 @@ def OPT(size, pages):
             pass
         elif page not in memoryQueue and len(memoryQueue) == frameSize: # memory full and page replacement occurs
             fault += 1
-            listMem = list(memoryQueue)
+            listMem = list(memoryQueue) # Converts queue into workable list
             maxVal = 0
-            for item in listMem:
+            for item in listMem: # Iterate through options for replacement
                 distance = pagesSeq.find(item,i)
                 if distance == -1:
                     distance = len(pagesSeq)*1000
@@ -85,12 +94,12 @@ def OPT(size, pages):
     return fault
 
 def processSequence(numPages):
-    #TODO Fix numpages, numpages * c =/= character length
-    return str(random.randint(0, numPages * 10000000000000000000000000000000000000000000000000000000000000000))[0:numPages + 1]
+    ''' Generates random page sequence reference string'''
+    return str(random.randint(10**(numPages-1),(10**numPages)-1))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python paging.py [Frame Size] [Length of Reference String]")
+        print("Usage: python paging.py [Frame Size] [Length of Reference String]") # Usage example python paging.py 5 64
     else:
         main()
